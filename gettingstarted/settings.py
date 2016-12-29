cd9a59bb5aad030679c9f1f6a4712e449d07d34e
Snorrem1984 <hammero.moen@gmail.com>
2016-12-29 12:13:48 +0100

    removed print statment in settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -133,5 +133,3 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-print STATICFILES_DIRS
