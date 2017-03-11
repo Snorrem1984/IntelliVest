66c360321c558a722290c5ca18f1277ecaa43984
Snorrem1984 <hammero.moen@gmail.com>
2017-03-11 18:16:56 +0100

    Added slider on index

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,6 +11,10 @@ import intellivest.views
 
 urlpatterns = [
     url(r'^$', intellivest.views.index, name='index'),
+    url(r'^omoss', intellivest.views.omoss, name='omoss'),
+    url(r'^portefolje', intellivest.views.portefolje, name='portefolje'),
+    url(r'^kontaktsendt', intellivest.views.kontaktsendt, name='kontaktsendt'),
+    url(r'^kontakt', intellivest.views.kontakt, name='kontakt'),
     url(r'^db', intellivest.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- intellivest/views.py
+++ intellivest/views.py
@@ -2,11 +2,62 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .forms import ContactForm
+
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect
+from django.template import Context
+from django.template.loader import get_template
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+def portefolje(request):
+    return render(request, 'portefolje.html')
+
+def omoss(request):
+    return render(request, 'omoss.html')
+
+def kontaktsendt(request):
+    return render(request, 'kontaktsendt.html')
+
+def kontakt(request):
+
+	form_class = ContactForm(request.POST or None)
+
+	# print form_class
+	if request.method == 'POST':
+	 	if form_class.is_valid():
+			contact_name = form_class.cleaned_data['contact_name']
+			contact_email = form_class.cleaned_data['contact_email']
+			form_content= form_class.cleaned_data['content']
+
+            # Email the profile with the 
+            # contact information
+	        template = get_template('contact_template.txt')
+	        context = Context({
+	            'contact_name': contact_name,
+	            'contact_email': contact_email,
+	            'form_content': form_content,
+	        })
+
+	        content = template.render(context)
+
+	        email = EmailMessage(
+	            "New contact form submission",
+	            content,
+	            "admin@intellivest.no",
+	            ['admin@intellivest.no'],
+	            headers = {'Reply-To': contact_email }
+	            )
+	        email.send()
+	        return redirect('kontaktsendt')
+
+	return render(request, 'kontakt.html', {
+        'form': form_class,
+    })
 
 def db(request):
 
